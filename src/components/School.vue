3849fabe1f9519d70a417073e482a6ce22d31035
weiyishujia <1601689636@qq.com>
2023-07-04 18:46:27 +0800

    20230704

---
+++ src/components/School.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学校名称：{{name}}</h2>
+        <h2>学校地址：{{address}}</h2>
+        <button @click="showX">点我显示X</button>
+    </div>   
+</template>
+
+<script>
+    // 组件交互相关的代码（数据、方法等等）
+    export default {
+        name:'School',
+        data(){
+            return {
+                name:'尚硅谷',
+                address:'北京'
+            }
+        },
+        methods:{
+            showX(){
+                console.log(this.x);
+            }
+        }, 
+    }
+    
+</script>
+    
+<style>
+/* 组件的样式 */
+    .demo{
+        background-color: green;
+    }
+</style>
