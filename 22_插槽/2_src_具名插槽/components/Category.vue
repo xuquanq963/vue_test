ee9914ba2c605d8eb340ed0687b5ad741bafeb32
weiyishujia <1601689636@qq.com>
2023-07-23 19:05:24 +0800

    20230723_1

---
+++ 22_插槽/2_src_具名插槽/components/Category.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="category">
+    <h3>{{title}}分类</h3>
+    <!-- 定义一个插槽（挖个坑，等着组件的使用者进行填充） -->
+    <slot name="center">我是一些默认值，当使用者没有传递具体结构时，我会出现</slot>
+    <slot name="footer">我是一些默认值，当使用者没有传递具体结构时，我会出现</slot>
+
+  </div>
+</template>
+
+<script>
+export default {
+    nmae:'Category',
+    props:['title']
+}
+</script>
+
+<style scoped>
+    .category{
+        width: 200px;
+        height: 300px;
+        background-color: skyblue;
+
+    }
+    h3{
+        text-align: center;
+        background-color: orange;
+    }
+    img{
+        width: 100%;
+    }
+</style>
